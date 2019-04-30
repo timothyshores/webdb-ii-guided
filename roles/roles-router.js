51b5398cf885a64420258f8c6b944563cf19e508
Timothy Shores <timothymshores@gmail.com>
2019-04-30 12:34:46 -0500

    Create a database with id and name using SQL Lite Studio. Install and configure knex. Create GET and POST requests to /api/roles

--- roles/roles-router.js
+++ roles/roles-router.js
@@ -1,28 +1,43 @@
+const knex = require('knex');
 const router = require('express').Router();
 
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+        filename: './data/rolex.db3'
+    },
+    useNullAsDefault: true
+};
+const db = knex(knexConfig);
+
 router.get('/', (req, res) => {
-  // get the roles from the database
-  res.send('Write code to retrieve all roles');
+    // return all roles from the database
+    db('roles')
+        .then(roles => res.status(200).json(roles))
+        .catch(err => console.log(err));
 });
 
 router.get('/:id', (req, res) => {
-  // retrieve a role by id
-  res.send('Write code to retrieve a role by id');
+    // retrieve a role by id
+    res.send('Write code to retrieve a role by id');
 });
 
 router.post('/', (req, res) => {
-  // add a role to the database
-  res.send('Write code to add a role');
+    // add a role to the database
+    db('roles')
+        .insert(req.body, ['name'])
+        .then(results => res.status(200).json(results))
+        .catch(err => res.status(500).json(err));
 });
 
 router.put('/:id', (req, res) => {
-  // update roles
-  res.send('Write code to modify a role');
+    // update roles
+    res.send('Write code to modify a role');
 });
 
 router.delete('/:id', (req, res) => {
-  // remove roles (inactivate the role)
-  res.send('Write code to remove a role');
+    // remove roles (inactivate the role)
+    res.send('Write code to remove a role');
 });
 
 module.exports = router;
